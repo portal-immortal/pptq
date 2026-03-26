fc7734ab93692aec86aec0266326240c6a3267e0
Maulana <thebhe86@gmail.com>
2026-03-26 15:32:02 +0700

    fix domain path for pptqnuruliman.ponpes.id and remove /pptq prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://portal-immortal.github.io',
-  base: '/pptq',
+  site: 'https://pptqnuruliman.ponpes.id',
+  base: '/',
   output: 'static',
 });
